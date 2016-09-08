47dd125799099252391ab007c9f7376a543ed6b2
Matthew Kavanagh <mattk@brandwatch.com>
2016-09-08 17:11:52 +0100

    Add regex URL include/exclude options for passthrough requests

--- index.js
+++ index.js
@@ -6,45 +6,64 @@ const url = require('url');
 const command = require('commander');
 
 command.description('Proxy HTTP requests with optional alterations')
-    .option('-p, --port [port]', 'listen on the given port')
-    .option('--delay [seconds]', 'wait (at least) the given number of seconds before responding')
-    .option('--status [code]', 'return the given HTTP status code for all requests')
-    .parse(process.argv);
+.option('-p, --port [port]', 'listen on the given port')
+.option('--delay [seconds]', 'wait (at least) the given number of seconds before responding')
+.option('--status [code]', 'return the given HTTP status code for all requests')
+.option('--include [regex]', 'alter only requests where the URL (/path?query) matches the given regex')
+.option('--exclude [regex]', 'exclude requests where the URL (/path?query) matches the given regex')
+.parse(process.argv);
 
 const target = url.parse(command.args[0]);
 
+const includeUrls = new RegExp(command.include || '.*');
+const excludeUrls = new RegExp(command.exclude || '.^');
+
 http.createServer((req, res) => {
-        const options = Object.assign({}, target);
+    const startTime = process.hrtime();
 
-        options.method = req.method;
-        options.path = url.parse(req.url).path;
-        options.headers = Object.assign({}, req.headers);
+    const shouldAlter = req.url.match(includeUrls.match) && !req.url.match(excludeUrls);
 
-        const upstream = http.request(options);
+    const info = {startTime, url: req.url, shouldAlter};
 
-        req.on('aborted', upstream.abort);
+    console.log('Sending request', info);
 
-        req.pipe(upstream);
+    const options = Object.assign({}, target);
 
-        upstream.end();
+    options.method = req.method;
+    options.path = url.parse(req.url).path;
+    options.headers = Object.assign({}, req.headers);
 
-        upstream.on('response', (response) => {
-                if (typeof command.status !== 'undefined') {
-                    res.statusCode = command.status;
-                } else {
-                    res.statusCode = response.statusCode;
-                }
+    const upstream = http.request(options);
 
-                Object.keys(response.headers).forEach((header) => {
-                    res.setHeader(header, response.headers[header]);
-                });
+    req.on('aborted', upstream.abort);
 
-                if (typeof command.delay !== 'undefined') {
-                    setTimeout(() => response.pipe(res), 1000 * command.delay);
-                } else {
-                    response.pipe(res);
-                }
-            })
-            .on('error', () => res.end('HTTP/1.1 400 Bad Request\r\n\r\n'));
-    })
-    .listen(command.port);
+    req.pipe(upstream);
+
+    upstream.end();
+
+    upstream.on('response', (response) => {
+        console.log(`Received response after ${process.hrtime(startTime)[0]} seconds`, info);
+
+        if (typeof command.status !== 'undefined' && shouldAlter) {
+            res.statusCode = command.status;
+        } else {
+            res.statusCode = response.statusCode;
+        }
+
+        Object.keys(response.headers).forEach((header) => {
+            res.setHeader(header, response.headers[header]);
+        });
+
+        const sendResponse = () => {
+            console.log(`Sending response after ${process.hrtime(startTime)[0]} seconds`, info);
+
+            response.pipe(res);
+        };
+
+        if (typeof command.delay !== 'undefined' && shouldAlter) {
+            setTimeout(sendResponse, 1000 * command.delay);
+        } else {
+            sendResponse();
+        }
+    }).on('error', () => res.end('HTTP/1.1 400 Bad Request\r\n\r\n'));
+}).listen(command.port);
